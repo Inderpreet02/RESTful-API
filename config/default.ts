53edc8e37fe3c1772393a22f84dfc744733d8230
Inderpreet02 <inder192002@gmail.com>
2022-08-26 00:04:17 +0530

    a pretty decent way

---
+++ config/default.ts
@@ -0,0 +1,6 @@
+export default{
+    port: 1337,
+    host: 'localhost',
+    dbUri: "mongodb+srv://root:root@cluster0.qr3b24q.mongodb.net/?retryWrites=true&w=majority",
+    saltWorkerFactor: 10,
+}
---
+++ src/controller/session.controller.ts
@@ -0,0 +1,12 @@
+export async function createUserSessionHandler( req: Request, res: Response ) {
+
+    // Validate the email and password they should be matching basically
+    
+    // Create a session
+
+    // create access token
+
+    // create access token
+
+    // 
+}
